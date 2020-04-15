0268378947b1323c2ac7fce80b4daffd2398c00f
victorfmesq <victorfmesq@gmail.com>
2020-04-15 12:14:24 -0300

    Add a script that move automatically and destoy some game object in scene

---
+++ Unity Features/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 2f;
+    public Vector2 direction;
+
+    public float lifeTime = 3f;
+
+    void Start()
+    {
+        // Destroy the bullet after some time
+        Destroy(this.gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+        Vector2 movement = direction.normalized * speed * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
